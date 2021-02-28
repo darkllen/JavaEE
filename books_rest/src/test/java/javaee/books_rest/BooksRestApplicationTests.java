54e4e94acdcd69ed6c75af0af12019441250d80a
darkllen <yankiniigor@gmail.com>
2021-02-28 18:15:30 +0200

    init lab

---
+++ books_rest/src/test/java/javaee/books_rest/BooksRestApplicationTests.java
@@ -0,0 +1,13 @@
+package javaee.books_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
