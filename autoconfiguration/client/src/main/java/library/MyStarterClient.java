4cf56ad33f372079988a1ab90ec7121281d44d99
darkllen <yankiniigor@gmail.com>
2021-03-07 21:00:58 +0200

    init

---
+++ autoconfiguration/client/src/main/java/library/MyStarterClient.java
@@ -0,0 +1,19 @@
+package library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyStarterClient {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(MyStarterClient.class, args);
+
+        MyLibraryClass myLibraryClassByClass = applicationContext.getBean(MyLibraryClass.class);
+        myLibraryClassByClass.printInfo();
+
+        MyLibraryClass myLibraryClass = (MyLibraryClass) applicationContext.getBean("myLibraryClass");
+        myLibraryClass.printInfo();
+    }
+}
